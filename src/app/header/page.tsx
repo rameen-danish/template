ae1f6865006703063befb663fac8498d0750a8a0
rameen-danish <rameenrashid15@gmail.com>
2024-10-25 05:48:50 +0500

    initial commit

---
+++ src/app/header/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function Header(){
+    return(
+        <div className="header">
+        <div className="header-button">
+            <div className="pr-10"><img src="Logo.png" alt="cyber-logo"/></div>
+            <div className="pr-10"><input type="text" size={40} maxLength={30} placeholder="Search" className="bg-gray-100 rounded p-3" /></div>
+            <ul>
+                <li className="text-black">Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Blog</li>
+            </ul>
+            <div className="icons">
+                <ul>
+                    <li><img src="Favorites.png" alt="fav-logo"/></li>
+                    <li><img src="Cart.png" alt="cart-logo"/></li>
+                    <li><img src="User.png" alt="user-logo"/></li>
+                </ul>
+             </div>
+        </div>
+    </div>
+    )
+}
---
+++ src/app/section1/page.tsx
@@ -0,0 +1,15 @@
+export default function Section1(){
+    return(
+        <div className="section">
+            <div className="content">
+            <h4>Pro.Beyond.</h4>
+            <h1>IPhone 14 <span>Pro</span></h1>
+            <h6>Created to change everything for the better. For everyone</h6>
+            <div ><button className="shop">Shop Now</button></div>
+           </div>
+           
+           <div className="image-container"><img src="Iphone Image.png" alt="iphone image"/></div>
+           
+        </div>
+    )
+}
